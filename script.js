68ba2afab696a321a2ffa4f982f7f39585013b2b
Joey Morgan <you@example.com>
2022-03-02 23:53:27 -0600

    Array

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 let id = 0;
+const books = [];
+
+
+ 
+function bookInfo() {
+    const bookObject = document.getElementById('books').value;
+const description = document.getElementById("Author", "Title", "Rating");
+const book = {
+    Author: Author,
+    Title: Title,
+    Rating: Rating,
+}
+console.log(bookObject);
+}
+
 document.getElementById('Add').addEventListener('click', () => {
     let table = document.getElementById('books');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
     row.insertCell(0).innerHTML=document.getElementById('Author').value; 
-    row.insertCell(1).innerHTML=document.getElementById('Book-Title').value; 
+    row.insertCell(1).innerHTML=document.getElementById('Title').value; 
     row.insertCell(2).innerHTML=document.getElementById("Rating").value;
     let action = row.insertCell(3);
     action.appendChild(createDeleteButton(id++));
     document.getElementById("Author").value = " ";
+    // const bookObject = document.getElementById('form').value;
+    const description = document.getElementById("Author", "Title", "Rating");
+    const book = {
+        Author: 'Author',
+        Title: 'Title',
+        Rating: 'Rating',
+    }
 
+
+    
 })
+
+
+
+
+
+
 
 function createDeleteButton(id){
     let btn = document.createElement('button');
@@ -24,3 +54,6 @@ function createDeleteButton(id){
     };
     return btn;
 }
+
+
+console.log(books);
